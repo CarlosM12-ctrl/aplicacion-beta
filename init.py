847d8be89b303f86b1022bd34479c3a430986daa
CarlosM12-ctrl <76485796+CarlosM12-ctrl@users.noreply.github.com>
2022-12-26 14:06:55 -0700

    cambio4

--- init.py
+++ init.py
@@ -1,6 +1,23 @@
 from tkinter import *
 from random import randint
+import tkinter
 #import pruebas as c
+
+class consul(tkinter.Toplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.state('zoomed')
+        self.resizable(1,1)
+        #self.isbn= IntVar()
+        #self.autor = StringVar()
+        #self.editorial = StringVar()
+        #self.titu =  StringVar()
+        self.dibujarComponentes()
+    
+    def dibujarComponentes(self):
+        Button(self, bg="red", text="Hola mundo", font=('nunito', 25, 'normal')).place(x=100, y=100)
+
+    
 
 class Interfaz():
     
@@ -31,16 +48,20 @@ class Interfaz():
         
         Label(self.ventana,text="Bienvenidx", bg="white", font=('Nunito', 40, "normal") ).place(x=880, y=30)
         Button(self.ventana,text="Solicitar Libro",bg="white",  border=0,font=('Nunito', 40, "normal") ).place(x=850,y=130)
-        Button(self.ventana,text="Buscar Libro",bg="white", border=0, font=('Nunito', 40, "normal") ).place(x=850,y=300)
+        Button(self.ventana,text="Buscar Libro",bg="white", command=self.nv, border=0, font=('Nunito', 40, "normal") ).place(x=850,y=300)
         Button(self.ventana,text="Información",bg="white",  border=0, font=('Nunito', 40, "normal") ).place(x=850,y=480)
+        Button(self.ventana,text="Información",bg="white",  border=0, font=('Nunito', 40, "normal") ).place(x=850,y=480)
+        
         #Text(self.ventana,text="Información",bg="white",  border=0, font=('Nunito', 40, "normal")).place(x=100,y=480)
       
         #img = PhotoImage(file="log.png")
-        self.image = PhotoImage(file="log.png")
+       # self.image = PhotoImage(file="log.png")
        # lbl_img = Label(self.ventana, image=img).place(x=10, y=100)
-        self.image = self.image.subsample(3)
-        Label(self.ventana, bg="#2E276C", image=self.image).place(x=150, y=50)
-       
+        #self.image = self.image.subsample(3)
+        #Label(self.ventana, bg="#2E276C", image=self.image).place(x=150, y=50)
+    def nv(self):
+        self.nventana: consul()
+
 
   
         
